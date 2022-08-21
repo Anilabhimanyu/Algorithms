dbce57ebcadf96dd8e3cd49309c62d2843b2bc41
anilKumar <anilkumarreddy3418@gmail.com>
2022-08-21 12:59:33 +0530

    second commit

---
+++ greedy.py
@@ -0,0 +1,13 @@
+#greedy approach
+
+nums=[int(x) for x in input().split()]
+total=int(input('enter the sum'))
+req=[]
+
+while total>0:
+  a=(total//max(nums))
+  for i in range(a):
+    req.append(max(nums))
+  total-=a*max(nums)
+  nums.remove(max(nums))
+print(req)
